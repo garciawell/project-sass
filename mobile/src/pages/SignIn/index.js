d2a063576132dbe4e651d990f311fd04c1f391b4
Wellington Garcia <garciawell@gmail.com>
2019-05-16 23:11:01 -0300

    att

---
+++ mobile/src/pages/SignIn/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import { View } from 'react-native';
+
+// import { Container } from './styles';
+
+const SignIn = () => <View />;
+
+export default SignIn;
--- mobile/src/routes.js
+++ mobile/src/routes.js
@@ -1,7 +1,13 @@
 import { createAppContainer, createSwitchNavigator } from 'react-navigation';
 
 import Main from '~/pages/Main';
+import SignIn from '~/pages/SignIn';
 
-const Routes = createAppContainer(createSwitchNavigator({ Main }));
+const Routes = createAppContainer(
+  createSwitchNavigator({
+    SignIn,
+    Main,
+  }),
+);
 
 export default Routes;
