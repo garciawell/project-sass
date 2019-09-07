a764718199be161e365f01bb268892cbabb2f94f
Wellington Garcia <garciawell@gmail.com>
2019-09-07 15:04:41 -0300

    Arquivo jestConfig

---
+++ frontend/jest.config.js
@@ -0,0 +1,13 @@
+module.export = {
+  roots: ['<rootDir>/src'],
+  transform: {
+    '\\.(js|jsx)?$': 'babel-jest',
+  },
+  testMatch: ['<rootDir>/src/**/>(*.)test.{js, ts, jsx, tsx}'],
+  moduleFileExtensions: ['js', 'jsx', 'json', 'node'],
+  testPathIgnorePatterns: ['/node_modules/', '/public/'],
+  setupFilesAfterEnv: [
+    'jest-dom/extend-expect',
+    'react-testing-library/cleanup-after-each',
+  ],
+};
